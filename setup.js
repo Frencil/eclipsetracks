52eaf203b4837b9d20f22a5b14d1c76230585495
Frencil <chris.clark@sparkfun.com>
2015-03-07 20:33:35 -0700

    loading all czml events from local .txt with a js module

--- setup.js
+++ setup.js
@@ -15,7 +15,53 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
 // Turn on day/night lighting
 viewer.scene.globe.enableLighting = true;
 
-// Load 2017-08-21 eclipse track data
-var czmlDataSource = new Cesium.CzmlDataSource();
-czmlDataSource.loadUrl('data/2017-08-21.czml');
-viewer.dataSources.add(czmlDataSource);
+// Module for loading and working with eclipses from local storage
+eclipses = {
+
+    czml_isos: new Array(),
+    czml_events: new Object(),
+    current_event_idx: -1,
+
+    load: function() {
+
+        // Initialize variables, read local storage
+        var frame = document.getElementById("czml_events");
+        var raw   = frame.contentWindow.document.body.childNodes[0].innerHTML;
+        var lines = raw.split("\n");
+        
+        // Parse czml event data into usable array of objects
+        for (var i = 0; i < lines.length; i++) {
+
+            if (lines[i].indexOf(",") == -1) { continue; }
+
+            // Parse the line into a fully formed object
+            var parts = lines[i].split(",");
+            var date  = new Date(parts[0]);
+            var iso   = date.toISOString().substr(0,10);
+            var czml  = 'data/' + iso + '.czml';
+            var event = { date: date, url: parts[1], czml: czml };
+
+            // Store events in an associative array with an iso index for walking
+            this.czml_isos.push(iso);
+            this.czml_events[iso] = event;
+
+        }
+
+        // Render the most current event
+        this.render(this.current());
+        
+    },
+
+    current: function(){
+        return this.czml_isos[this.czml_isos.length-1];
+    },
+
+    render: function(iso){
+        var czml = this.czml_events[iso].czml;
+        var czmlDataSource = new Cesium.CzmlDataSource();
+        czmlDataSource.loadUrl(czml);
+        viewer.dataSources.add(czmlDataSource);
+    }
+    
+};
+
